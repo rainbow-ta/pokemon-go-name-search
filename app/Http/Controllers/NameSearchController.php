c407401881115d11d048200c2734983271f8341c
kamikitakamikita <ebinakota.h@gmail.com>
2022-10-18 22:16:19 +0900

    DBから取得した検索ワードをビューへ表示する

--- app/Http/Controllers/NameSearchController.php
+++ app/Http/Controllers/NameSearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SearchWord;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class NameSearchController extends Controller
@@ -9,6 +11,7 @@ class NameSearchController extends Controller
     public function index()
     {
         return Inertia::render('NameSearch/NameSearchIndex', [
+            'searchWords' => SearchWord::where('user_id', Auth::id())->get(),
         ]);
     }
 }
--- resources/js/Pages/NameSearch/NameSearchIndex.vue
+++ resources/js/Pages/NameSearch/NameSearchIndex.vue
@@ -1,5 +1,12 @@
 <script setup>
 import ComponentsHeader from '@/Components/ComponentsHeader.vue';
+
+defineProps({
+  searchWords: {
+    type: Array,
+    required: true,
+  },
+});
 </script>
 
 <template>
@@ -24,12 +31,15 @@ import ComponentsHeader from '@/Components/ComponentsHeader.vue';
               コピー
             </th>
             <th class="w-10/12 px-4 py-2">
-              キーワード名
+              保存した検索ワード
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr
+            v-for="searchWord in searchWords"
+            :key="searchWord.id"
+          >
             <td class="border px-4 py-2">
               <div class="form-check flex justify-center">
                 <input
@@ -44,43 +54,7 @@ import ComponentsHeader from '@/Components/ComponentsHeader.vue';
               <span class="cursor-pointer"><font-awesome-icon icon="fa-solid fa-copy" /></span>
             </td>
             <td class="border px-4 py-2">
-              BOX整理定型
-            </td>
-          </tr>
-          <tr class="bg-gray-200">
-            <td class="border px-4 py-2">
-              <div class="form-check flex justify-center">
-                <input
-                  id="flexCheckDefault3"
-                  class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
-                  type="checkbox"
-                  value=""
-                >
-              </div>
-            </td>
-            <td class="border px-4 py-2 flex justify-center">
-              <span class="cursor-pointer"><font-awesome-icon icon="fa-solid fa-copy" /></span>
-            </td>
-            <td class="border px-4 py-2">
-              GBL
-            </td>
-          </tr>
-          <tr>
-            <td class="border px-4 py-2">
-              <div class="form-check flex justify-center">
-                <input
-                  id="flexCheckDefault3"
-                  class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
-                  type="checkbox"
-                  value=""
-                >
-              </div>
-            </td>
-            <td class="border px-4 py-2 flex justify-center">
-              <span class="cursor-pointer"><font-awesome-icon icon="fa-solid fa-copy" /></span>
-            </td>
-            <td class="border px-4 py-2">
-              レアポケ
+              {{ searchWord.name }}
             </td>
           </tr>
         </tbody>
