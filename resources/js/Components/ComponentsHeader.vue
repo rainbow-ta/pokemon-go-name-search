b6da5ae3e906f4227fb93bc374ff65e24f7d2943
kamikitakamikita <ebinakota.h@gmail.com>
2022-10-18 13:32:04 +0900

    ヘッダーの作成

--- resources/js/Components/ComponentsHeader.vue
+++ resources/js/Components/ComponentsHeader.vue
@@ -8,18 +8,11 @@
           class="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5"
           aria-label="logo"
         >
-          <svg
-            width="95"
-            height="94"
-            viewBox="0 0 95 94"
-            class="w-6 h-auto text-indigo-500"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
+          <img
+            src="https://img.icons8.com/dusk/64/000000/pokemon-go.png"
+            alt="ポケモンGOのアイコン"
           >
-            <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-          </svg>
-
-          Flowrift
+          PokemonGO Name Search
         </a>
         <!-- logo - end -->
 
