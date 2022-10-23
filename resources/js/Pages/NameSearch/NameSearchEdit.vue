40fcba3b1367c61fba4aabebbb32474b7d12ac3b
kamikitakamikita <ebinakota.h@gmail.com>
2022-10-23 19:05:15 +0900

    アンド検索のUI追加

--- resources/js/Pages/NameSearch/NameSearchEdit.vue
+++ resources/js/Pages/NameSearch/NameSearchEdit.vue
@@ -112,19 +112,31 @@ const makeImagePath = pokedexNo => {
         v-for="pokemon in pokemons"
         :key="pokemon.pokedex_no"
         for=""
-        class="flex items-center"
       >
-        <input
-          id="flexCheckDefault3"
-          class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
-          type="checkbox"
-          value=""
-        >
-        <img
-          :alt="pokemon.name"
-          :src="makeImagePath(pokemon.pokedex_no)"
-        >
-        <span>{{ pokemon.name }}</span>
+        <div class="flex items-center">
+          <input
+            id="flexCheckDefault3"
+            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+            type="checkbox"
+            value=""
+          >
+          <img
+            :alt="pokemon.name"
+            :src="makeImagePath(pokemon.pokedex_no)"
+          >
+          <span>{{ pokemon.name }}</span>
+        </div>
+        <div class="ml-5">
+          <label for="">
+            <input
+              id="flexCheckDefault3"
+              class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
+              type="checkbox"
+              value=""
+            >
+            <span class="ml-3">&検索</span>
+          </label>
+        </div>
       </label>
     </div>
   </div>
