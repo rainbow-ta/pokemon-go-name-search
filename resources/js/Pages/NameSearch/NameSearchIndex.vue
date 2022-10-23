356e10de2043c81377f12ecf8593bf8a32fa5a7b
kamikitakamikita <ebinakota.h@gmail.com>
2022-10-23 21:30:23 +0900

    UIの修正

--- resources/js/Pages/NameSearch/NameSearchIndex.vue
+++ resources/js/Pages/NameSearch/NameSearchIndex.vue
@@ -12,12 +12,34 @@ defineProps({
 <template>
   <ComponentsHeader />
   <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-    <div class="mb-5">
+    <div class="flex items-center mb-5">
+      <input
+        id="exampleSearch"
+        type="search"
+        class="
+          form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+          mr-5
+          w-96
+        "
+        placeholder="タグ名を入力してください"
+      >
+
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5">
-        Button
-      </button>
-      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-        Button
+        保存
       </button>
     </div>
     <div class="flex justify-center">
@@ -25,13 +47,10 @@ defineProps({
         <thead class="bg-gray-50">
           <tr>
             <th class="w-1/12 px-4 py-2">
-              選択
-            </th>
-            <th class="w-1/12 px-4 py-2">
               コピー
             </th>
             <th class="w-10/12 px-4 py-2">
-              保存した検索ワード
+              保存済みのタグ
             </th>
           </tr>
         </thead>
@@ -40,16 +59,6 @@ defineProps({
             v-for="searchList in searchLists"
             :key="searchList.id"
           >
-            <td class="border px-4 py-2">
-              <div class="form-check flex justify-center">
-                <input
-                  id="flexCheckDefault3"
-                  class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer"
-                  type="checkbox"
-                  value=""
-                >
-              </div>
-            </td>
             <td class="border px-4 py-2 flex justify-center">
               <span class="cursor-pointer"><font-awesome-icon icon="fa-solid fa-copy" /></span>
             </td>
