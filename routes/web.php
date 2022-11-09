c2bddffd21f5cf365419e498a423f68c0d917d86
kamikitakamikita <ebinakota.h@gmail.com>
2022-11-09 21:58:41 +0900

    不要なルーティングを削除

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,26 +14,12 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
+// TODO:トップページに"/"でもアクセス出来るようにする
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
-
-Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource(
         '/name-search',
         // TODO:名前付きルートのより良い記述があればリファクタリングする
